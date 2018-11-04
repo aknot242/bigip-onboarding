5226c36f0b3c003bad87a91fbabaf39198b43dd7
Daniel Edgar <aknot242@gmail.com>
2018-11-04 08:39:27 -0800

    using relative home directory for volume mount argument

--- onboard.sh
+++ onboard.sh
@@ -2,4 +2,4 @@
 docker build -t aknot242/f5-rs-container .
 
 #start container
-docker run -v /Users/edgar/Documents/workspaces/bigip-onboarding:/home/snops/bigip-onboarding -p 2222:22 -p 10000:8080 -it --name bigip_onboard --rm aknot242/f5-rs-container:latest
+docker run -v $(pwd):/home/snops/bigip-onboarding -p 2222:22 -p 10000:8080 -it --name bigip_onboard --rm aknot242/f5-rs-container:latest
